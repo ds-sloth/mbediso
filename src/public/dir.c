19227b5ef467b6e65fe1cbef8e39c8a5fcc2c72f
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-07-08 01:26:30 -0400

    Add initial dir API

---
+++ src/public/dir.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "public/dir.h"
+#include "internal/io.h"
+#include "internal/fs.h"
+#include "internal/directory.h"
+
+struct mbediso_dir* mbediso_opendir(struct mbediso_fs* fs, const char* name)
+{
+    const struct mbediso_dir_entry* dir_entry  = mbediso_fs_lookup(fs, name, strlen(name));
+    if (!dir_entry
+        || !dir_entry->directory
+        || dir_entry->length != 0
+        || dir_entry->sector >= fs->directory_count)
+    {
+        return NULL;
+    }
+
+    const struct mbediso_directory* directory = &fs->directories[dir_entry->sector];
+
+    struct mbediso_dir* dir = malloc(sizeof(struct mbediso_dir));
+    if (!dir)
+        return NULL;
+
+    dir->fs = fs;
+    dir->directory = directory;
+    dir->entry_index = 0;
+    return dir;
+}
+
+int mbediso_closedir(struct mbediso_dir* dir)
+{
+    free(dir);
+    return 0;
+}
+
+struct mbediso_dirent* mbediso_readdir(struct mbediso_dir* dir)
+{
+    if (dir->entry_index >= dir->directory->entry_count)
+        return NULL;
+
+    const struct mbediso_dir_entry* entry = &dir->directory->entries[dir->entry_index];
+
+    dir->dirent.d_type = entry->directory ? MBEDISO_DT_DIR : MBEDISO_DT_REG;
+
+    int name_res = mbediso_string_diff_reconstruct(
+        dir->dirent.d_name,
+        sizeof(dir->dirent.d_name),
+        dir->directory->stringtable,
+        dir->directory->entries,
+        dir->directory->entry_count,
+        sizeof(struct mbediso_dir_entry),
+        dir->entry_index
+    );
+    if (name_res != 0)
+    {
+        // This should be unreachable
+        return NULL;
+    }
+
+    dir->entry_index++;
+    return &dir->dirent;
+}
---
+++ src/public/dir.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+
+#define MBEDISO_DT_UKNOWN 1
+#define MBEDISO_DT_REG 2
+#define MBEDISO_DT_DIR 3
+
+struct mbediso_fs;
+struct mbediso_directory;
+
+struct mbediso_dirent
+{
+    uint8_t d_name[334];
+    int d_type;
+};
+
+struct mbediso_dir
+{
+    struct mbediso_fs* fs;
+    const struct mbediso_directory* directory;
+    struct mbediso_dirent dirent;
+    uint32_t entry_index;
+};
+
+struct mbediso_dir* mbediso_opendir(struct mbediso_fs* fs, const char* name);
+
+int mbediso_closedir(struct mbediso_dir* dir);
+
+struct mbediso_dirent* mbediso_readdir(struct mbediso_dir* dir);
