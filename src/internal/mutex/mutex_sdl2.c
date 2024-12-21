12aed30e611c0ce819e5d6b84d0830806e0ae95c
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-12-21 11:25:39 -0500

    Add support for SDL2 mutexes and application-provided mutexes

---
+++ src/internal/mutex/mutex_sdl2.c
@@ -0,0 +1,42 @@
+/*
+ * mbediso - a minimal library to load data from compressed ISO archives
+ *
+ * Copyright (c) 2024 ds-sloth
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "internal/mutex/mutex.h"
+
+#include "SDL2/SDL_mutex.h"
+
+mbediso_mutex_t mbediso_mutex_alloc()
+{
+    return (mbediso_mutex_t)SDL_CreateMutex();
+}
+
+void mbediso_mutex_free(mbediso_mutex_t mutex)
+{
+    SDL_DestroyMutex((SDL_mutex*)mutex);
+}
+
+void mbediso_mutex_lock(mbediso_mutex_t mutex)
+{
+    SDL_LockMutex((SDL_mutex*)mutex);
+}
+
+void mbediso_mutex_unlock(mbediso_mutex_t mutex)
+{
+    SDL_UnlockMutex((SDL_mutex*)mutex);
+}
